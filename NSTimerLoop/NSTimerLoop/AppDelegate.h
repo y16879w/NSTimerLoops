58d87f12df94bb65cde0d362675576d354e382fc
liyanmin <liyanmin3@jd.com>
2018-12-22 15:00:10 +0800

    init commit

---
+++ NSTimerLoop/NSTimerLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSTimerLoop
+//
+//  Created by 李艳敏 on 2018/12/22.
+//  Copyright © 2018年 李艳敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
